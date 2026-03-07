da4927093dc58d181f8dcd7472f5f8f1c8643224
issa146 <issaissaadi95@gmail.com>
2026-03-07 17:33:25 +0100

    je suis entrain de fini le voyage controller

--- src/Controller/VoyageController.php
+++ src/Controller/VoyageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\VoyageRepository;
 use App\Service\UnsplashService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,49 +13,64 @@ use Symfony\Component\Routing\Attribute\Route;
 final class VoyageController extends AbstractController
 {
     #[Route('/voyage', name: 'app_voyage')]
-    public function index(Request $request, UnsplashService $unsplash): Response
+    public function index(Request $request, VoyageRepository $voyageRepository, UnsplashService $unsplash, EntityManagerInterface $entityManager): Response
     {
         
 
-        $destination = trim((string) $request->query->get('destination', 'paris')); # je récupere les parmètere GET  
+        $destination = trim((string) $request->query->get('destination', '')); # je récupere les parmètere GET  
 
-        $imageUrl = $unsplash->getImageUrl($destination) ?? ''; # j'appel mon service avec la destination
+        // $imageUrl = $unsplash->getImageUrl($destination) ?? ''; # j'appel mon service avec la destination
 
-        $cards = [];
-            for ($i = 1; $i <= 12; $i++) {
-                $cards[] = [
-                'id' => $i,
-                'destination' => $destination,
-                'formule' => 'basic',
-                'prix' => 210,
-                'image' => $imageUrl,
-                ];
-                
+        # si la bar de recherch n'est pas vide
+        if($destination !== '') {
+            $voyages = $voyageRepository->createQueryBuilder('v')  
+            ->where('LOWER(v.destination) LIKE :destination') # je utilise la fonction LOWER pour metre tous en minuscules coté bdd et LIKE pour
+            ->setParameter('destination', '%' . mb_strtolower($destination) . '%' ) # je donne une valeur au paramètre :destination pour protège contre les injections SQL
+            ->orderBy('v.destination', 'ASC') 
+            ->getQuery()
+            ->getResult();
+        } else {
+            $voyages = $voyageRepository->findBy([], ['destination' => 'ASC']);
+        }
+
+
+        $modification = false; # je initialises un flag pour savoir si j'ai modifié au moins un voyage
+
+        foreach ($voyages as $voyage) {
+            if(!$voyage->getImageUrl()) {
+                $imageUrl = $unsplash->getImageUrl($voyage->getDestination());
+
+                if($imageUrl) {
+                    $voyage->setImageUrl($imageUrl);
+                    $modification = true;
+                }
             }
+        }
+
+        if($modification) {
+            $entityManager->flush();
+        }
 
             return $this->render('voyage/index.html.twig', [
             'destination' => $destination,
-            'cards' => $cards
-
+            'vo$voyages' =>$voyages   
         ]);     
 
     }
 
 
     #[Route('/voyage/{destination}', name: 'voyage_detail', requirements:['destination' => '.+'])]
-    public function detail(string $destination, UnsplashService $unsplash): Response {
-        $destination = trim($destination);
+    public function detail(string $destination, VoyageRepository $voyageRepository) {
+        
 
-        $imageUrl = $unsplash->getImageUrl($destination); # j'appel mon service Unsplash
+    $voyage = $voyageRepository->findOneBy(['destination' => $destination]);
 
-        $formule = 'basic';
-        $prix = 210;
+    if (!$voyage) {
+        throw $this->createNotFoundException('Voyage introuvable');
+    }
 
         return $this->render('voyage/detail.html.twig', [
-            'destination' => $destination,
-            'imageUrl' => $imageUrl,
-            'formule' => $formule,
-            'prix' => $prix,
+            'voyage' => $voyage,
         ]); 
             
             
