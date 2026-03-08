9ba589a35e1bff02ecc0c13b8d29b4b7d8c99d28
issa146 <issaissaadi95@gmail.com>
2026-03-08 16:54:10 +0100

    j'ai fini le voyage controller

--- src/Controller/VoyageController.php
+++ src/Controller/VoyageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Voyage;
 use App\Repository\VoyageRepository;
 use App\Service\UnsplashService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -23,32 +24,32 @@ final class VoyageController extends AbstractController
 
         # si la bar de recherch n'est pas vide
         if($destination !== '') {
-            $voyages = $voyageRepository->createQueryBuilder('v')  
-            ->where('LOWER(v.destination) LIKE :destination') # je utilise la fonction LOWER pour metre tous en minuscules coté bdd et LIKE pour
-            ->setParameter('destination', '%' . mb_strtolower($destination) . '%' ) # je donne une valeur au paramètre :destination pour protège contre les injections SQL
-            ->orderBy('v.destination', 'ASC') 
-            ->getQuery()
-            ->getResult();
+            $voyages = $voyageRepository->createQueryBuilder('v')  # je construie une requet pour l'entité voyage
+            ->where('LOWER(v.destination) LIKE :destination')   # je utilise la fonction LOWER pour metre tous en minuscules coté bdd et LIKE pour rechercher exactement la destination
+            ->setParameter('destination', '%' . mb_strtolower($destination) . '%' )     # je donne une valeur au paramètre :destination pour protège contre les injections SQL
+            ->orderBy('v.destination', 'ASC')   # je fait un tri sur la colonne destination de de l'entité v
+            ->getQuery()     # je prépare le QueryBuilde requet à ètre excuté 
+            ->getResult();   # j'exécutes la requête sous forme de tableau d'objet
         } else {
-            $voyages = $voyageRepository->findBy([], ['destination' => 'ASC']);
+            $voyages = $voyageRepository->findBy([], ['destination' => 'ASC']); # je récupère tous les voyage
         }
 
 
         $modification = false; # je initialises un flag pour savoir si j'ai modifié au moins un voyage
 
         foreach ($voyages as $voyage) {
-            if(!$voyage->getImageUrl()) {
-                $imageUrl = $unsplash->getImageUrl($voyage->getDestination());
+            if(!$voyage->getImageUrl()) { # je vérifie si le voyage n'a aucune image en bdd
+                $imageUrl = $unsplash->getImageUrl($voyage->getDestination()); # j'appelles mon service et je lui passe la destination 
 
-                if($imageUrl) {
-                    $voyage->setImageUrl($imageUrl);
-                    $modification = true;
+                if($imageUrl) { # je vérifie si $imageUrl n'est pas vide 
+                    $voyage->setImageUrl($imageUrl); # j'appelle le setter et je lui passe l'image
+                    $modification = true; # j'indique que au moins un voyage a été modifié
                 }
             }
         }
 
-        if($modification) {
-            $entityManager->flush();
+        if($modification) { # je vérifi si est $modification true 
+            $entityManager->flush(); # je envoie toutes les modif en bdd
         }
 
             return $this->render('voyage/index.html.twig', [
@@ -59,16 +60,9 @@ final class VoyageController extends AbstractController
     }
 
 
-    #[Route('/voyage/{destination}', name: 'voyage_detail', requirements:['destination' => '.+'])]
-    public function detail(string $destination, VoyageRepository $voyageRepository) {
-        
-
-    $voyage = $voyageRepository->findOneBy(['destination' => $destination]);
-
-    if (!$voyage) {
-        throw $this->createNotFoundException('Voyage introuvable');
-    }
-
+    #[Route('/voyage/{id}', name: 'voyage_detail', requirements:['id' => '\d+'])]
+    public function detail(Voyage $voyage): Response {
+ 
         return $this->render('voyage/detail.html.twig', [
             'voyage' => $voyage,
         ]); 
